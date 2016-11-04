24d84d5b1e25878e2e1d4594da7732795121877c
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-04 10:33:32 -0700

    finish triple size double

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -2,8 +2,9 @@ require 'warmup'
 require 'spec_helper'
 
 describe Warmup do
+  let(:warmup) { Warmup.new }
+
   describe '#gets_shout' do
-    let(:warmup) { Warmup.new }
 
     it 'returns a capitalized message based on user input' do
       allow(warmup).to receive(:gets).and_return("hello")
@@ -12,4 +13,14 @@ describe Warmup do
 
   end
 
+  describe '#triple_size' do
+    it "returns triple the size" do 
+      array_double = double(:size => 6)
+      expect(warmup.triple_size(array_double)).to eq(18)
+    end
+  end  
+
+  
+
 end
+
