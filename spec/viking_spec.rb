e5ba836de0ef8633f9707f47bc446cb88bb99e89
Jake Tingley <jaket32@gmail.com>
2016-11-04 19:37:32 -0400

    continued viking tests

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 
 describe Viking do
   let(:viking) { Viking.new('Bob', 150) }
+  let(:victim) { Viking.new('Greg', 100) }
 
 
   describe "#initialize" do
@@ -33,9 +34,9 @@ describe Viking do
     end    
 
     it "exception is raised when picking up an invalid weapon" do
-          
 
       expect{viking.pick_up_weapon('lightsabre')}.to raise_error(RuntimeError)
+
     end    
 
     it "Viking can pick up a weapon" do
@@ -69,13 +70,40 @@ describe Viking do
       expect(viking.health).to eq(147)
     end
 
-   it "calls the take_damage method" do
+    it "calls the take_damage method" do
       
+      expect(viking).to receive(:take_damage).with(5)
+      viking.receive_attack(5)    
+
     end
 
   end 
 
-  describe ""
+  describe '#attack' do
+
+    it 'lowers opponents health when they are attacked' do
+
+      viking.attack(victim)
+
+      expect(victim.health).to be < 100
+    
+    end
+
+    it 'attacking another viking calls that viking\'s take damage method' do
+      expect(victim).to receive(:take_damage)
+      viking.attack(victim)
+    end
+
+    # it 'calls damage with fist when attacks with no weapon' do
+    #   # viking.attack(victim)
+    #   expect(viking.attack(victim)).to receive(:damage_with_fists)
+    # end
+
+    it 'calls damage with fists when attacking with no weapon' do
+      expect(viking).to receive(:damage_with_fists)
+      viking.attack(victim)
+    end
+  end
 
 
 
